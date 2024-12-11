e4c239a56940c67dd362b5f245e2642928d0776e
Padma Radhika Upadhyayula <125632326+PadmaRadhika@users.noreply.github.com>
2024-12-10 19:13:44 -0800

    Update FeignService.java
    Added EC2 Url in FeignService.java

--- src/main/java/com/example/fooddeliverypayments/config/FeignService.java
+++ src/main/java/com/example/fooddeliverypayments/config/FeignService.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.example.fooddeliverypayments.model.Order;
 
 
-@FeignClient(name="FOODDELIVERYORDERS", url= "http://localhost:8084/api/orders")
+@FeignClient(name="FOODDELIVERYORDERS", url= "http://ec2-35-171-160-2.compute-1.amazonaws.com:8084/api/orders")
 public interface FeignService {
 	@PutMapping("update")
 	public ResponseEntity<Order> updateOrder(@RequestBody Order updateData);
